582bb0e2e5221dc0bbdc5e583aae22e4c7d43806
zeitgeist117 <zeitgeist117@proton.me>
2023-11-25 00:01:14 +1100

    Changes to be committed: 	deleted:    README.md 	modified:   keymap.c
    Untracked files:
    	README.org

--- README.md
+++
@@ -1,1 +0,0 @@
-# CRKBD QMK Keymap
--- keymap.c
+++ keymap.c
@@ -26,6 +26,8 @@ enum {
     TD_ENT_TAB,
 };
 
+
+
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
@@ -47,20 +49,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  TD(TD_ENT_TAB),
+      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, TT(1),  KC_SPC,       KC_SPC,   MO(2),  KC_RALT
+                                          KC_LGUI, MO(1),  KC_SPC,       KC_SPC,   LT(2, KC_LSFT),  KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_TILD,    KC_1,    KC_2,    KC_3,  KC_EQL,                      KC_CIRC, KC_AMPR, KC_LCBR, KC_RCBR, KC_MINS, KC_BSPC,
+       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,  KC_5,                      KC_CIRC, KC_AMPR, KC_LCBR, KC_RCBR, KC_MINS, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_PIPE,    KC_4,    KC_5,    KC_6, KC_MINS,                      KC_LPRN,  KC_RPRN, KC_LBRC, KC_RBRC, KC_GRV, KC_BSLS,
+      KC_LSFT,    KC_6,    KC_7,    KC_8,    KC_9, KC_0,                      KC_LPRN,  KC_RPRN, KC_LBRC, KC_RBRC, KC_GRV, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_DEL,    KC_7,  KC_8,   KC_9,    KC_0,                      KC_UNDS, KC_PLUS, KC_ASTR, KC_PSCR, XXXXXXX, XXXXXXX,
+      KC_LCTL,    KC_DEL,    KC_PIPE,  KC_MINS,   KC_EQL,    KC_TILD,                      KC_UNDS, KC_PLUS, KC_ASTR, KC_PSCR, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_SPC, MO(3), KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -70,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, KC_F1,   KC_F2, KC_F3,  KC_F4, KC_EXLM,                               KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_ASTR, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_F5,   KC_F6,    KC_F7,  KC_F8,   KC_AT,                        KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_VOLD, KC_VOLU,
+      KC_LSFT, KC_F5,   KC_F6,    KC_F7,  KC_F8,   KC_AT,                        KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_VOLD, KC_VOLU,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_F9, KC_F10, KC_F11, KC_F12, KC_HASH,                      XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+      KC_LCTL, KC_F9, KC_F10, KC_F11, KC_F12, KC_HASH,                      XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),  KC_ENT,     KC_SPC,  _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
